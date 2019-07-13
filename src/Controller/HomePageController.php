e34ef8e6291398bd9b2617f58882c8cac653e036
datnv <datnv030691@gmail.com>
2019-07-13 21:34:11 +0900

    contact page

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -4,8 +4,13 @@
 namespace App\Controller;
 
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class HomePageController extends Controller
 {
@@ -26,9 +31,48 @@ class HomePageController extends Controller
 
     /**
      * @Route("/contact", name="contact")
+     * @Method("GET|POST")
      */
-    public function contact() {
-        return $this->render('contact/contact.html.twig');
+    public function contact(Request $request, \Swift_Mailer $mailer) {
+        $enquiry = new Contact();
+        $form = $this->createForm(ContactType::class, $enquiry);
+
+        $this->request = $request;
+        if ($request->getMethod() == 'POST') {
+//            $form->bind($request);
+            $data = $form->handleRequest($request);
+
+
+
+            if ($form->isValid()) {
+                // Perform some action, such as sending an email
+
+//                $sub = $data->get("contact");
+
+//                var_dump($enquiry);exit;
+
+
+
+                $message = new \Swift_Message();
+                $message->setSubject('Contact enquiry from Example')
+                    ->setFrom('contact@example.com')
+                    ->setTo('datnv030691@gmail.com')
+                    ->setBody($this->renderView('contact/contactEmail.txt.twig', array('enquiry' => $enquiry)));
+
+
+                $mailer->send($message);
+
+//                $this->get('session')->getFlashBag('blog-notice', 'Your contact enquiry was successfully sent. Thank you!');
+
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('contact'));
+            }
+        }
+
+        return $this->render('contact/contact.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 
     /**
